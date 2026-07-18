27f716053a9079ce977b3bb76b859689f00e705b
sayandweep <sayandweepbiswas@gmail.com>
2026-07-18 20:23:28 +0530

    Production ready

--- server/app.js
+++ server/app.js
@@ -7,7 +7,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://transship.sayandweep.in"
+  ]
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
