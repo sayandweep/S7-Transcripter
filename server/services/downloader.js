2aba5d92f79a9b9363310edc6c2123dc700da069
sayandweep <sayandweepbiswas@gmail.com>
2026-07-21 19:46:19 +0530

    new change v3

--- server/services/downloader.js
+++ server/services/downloader.js
@@ -6,14 +6,16 @@ import crypto from "crypto";
 
 const execFileAsync = promisify(execFile);
 
-export async function downloadAudio(url) {
+export async function downloadMedia(url) {
   const id = crypto.randomUUID();
 
   const outputTemplate = path.resolve("temp", `${id}.%(ext)s`);
 
   await execFileAsync("yt-dlp", [
     "-f",
-    "bestaudio",
+    "bv*+ba/b",
+    "--merge-output-format",
+    "mp4",
     "--no-playlist",
     "-o",
     outputTemplate,
@@ -25,13 +27,13 @@ export async function downloadAudio(url) {
   const file = files.find((f) => f.startsWith(id));
 
   if (!file) {
-    throw new Error("Downloaded audio not found");
+    throw new Error("Downloaded media not found");
   }
 
   return path.resolve("temp", file);
 }
 
-export function deleteAudio(filePath) {
+export function deleteMedia(filePath) {
   if (fs.existsSync(filePath)) {
     fs.unlinkSync(filePath);
   }
